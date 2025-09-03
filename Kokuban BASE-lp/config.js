5202ac745cb9fc540b3b91b640689f7d68c69684
borkenrdio <yamada@idea-spot.net>
2025-09-03 16:12:53 +0900

    Add files via upload

---
+++ Kokuban BASE-lp/config.js
@@ -0,0 +1,4 @@
+// microCMSの接続情報を設定
+// 以下の '' の中に、ご自身のmicroCMSの情報を入力してください
+const MICROCMS_SERVICE_DOMAIN = 'kokubanbase'; // 例: 'kokuban-base'
+const MICROCMS_API_KEY = '1Mr8JvDoCxPQa6s3lIAJOwP78xM9AnEQd67v'; // 例: 'xxxxxxxxxxxx'
